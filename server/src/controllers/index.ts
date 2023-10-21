3b6f90acd0eec6d9c4fdffb26cdff8b6bea09f60
Piotrek <Swistekxd@gmail.com>
2023-10-22 00:49:34 +0200

    first commit

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+/**
+ * GET /
+ * Home page.
+ */
+export const index = async (req: Request, res: Response): Promise<void> => {
+    // res.render("index", { title: "Express" });
+    res.json("oasdask").status(200);
+};
